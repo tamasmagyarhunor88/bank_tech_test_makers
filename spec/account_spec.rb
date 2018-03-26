6ff09201b4647e9b5b08ad70361637d710448b56
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-03-26 11:57:38 +0100

    Add test, pass test: Account Holder can withdraw from bank account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,17 +1,34 @@
 require 'account'
+
 describe Account do
   my_account = Account.new
 
-  it 'responds to #balance' do
-    expect(my_account).to respond_to(:balance)
+  context 'initialize' do
+    it 'responds to #balance' do
+      expect(my_account).to respond_to(:balance)
+    end
   end
 
-  it 'responds to #deposit' do
-    expect(my_account).to respond_to(:deposit)
+  context 'deposit' do
+    it 'responds to #deposit' do
+      expect(my_account).to respond_to(:deposit)
+    end
+
+    it 'can deposit amount to account' do
+      my_account.deposit(100)
+      expect(my_account.balance).to eq(100)
+    end
   end
 
-  it 'can deposit amount to account' do
-    my_account.deposit(100)
-    expect(my_account.balance).to eq(100)
+  context 'withdraw' do
+    it 'responds to #withdraw' do
+      expect(my_account).to respond_to(:withdraw)
+    end
+
+    it 'can #withdraw amount from account' do
+      my_account.withdraw(50)
+      expect(my_account.balance).to eq(50)
+    end
   end
+
 end
