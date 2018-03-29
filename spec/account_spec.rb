fdba3ead6c1b2460bcbe41dcd9feb6feca42b656
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-03-29 10:21:33 +0100

    Add Account class tests and pass

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,4 +2,15 @@ require 'account'
 
 describe Account do
 
+  before(:all) do
+    @my_account = Account.new
+  end
+
+  it 'responds to #balance' do
+    expect(@my_account).to respond_to(:balance)
+  end
+
+  it 'initializes with 0 balance' do
+    expect(@my_account.balance).to eq(0)
+  end
 end
