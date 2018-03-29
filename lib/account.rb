9ce673c50da97d429cfd6e3564d7050653c371cd
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-03-29 10:26:26 +0100

    Get rid of magic number for minimum balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,9 @@
 class Account
+
+  MINIMUM_BALANCE = 0
+
   attr_reader :balance
   def initialize
-    @balance = 0
+    @balance = MINIMUM_BALANCE
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,16 +1,16 @@
 require 'account'
 
 describe Account do
-
   before(:all) do
     @my_account = Account.new
+    @minimum_balance = Account::MINIMUM_BALANCE
   end
 
-  it 'responds to #balance' do
+  it "responds to #balance" do
     expect(@my_account).to respond_to(:balance)
   end
 
-  it 'initializes with 0 balance' do
-    expect(@my_account.balance).to eq(0)
+  it "initializes with minimum balance" do
+    expect(@my_account.balance).to eq(@minimum_balance)
   end
 end
