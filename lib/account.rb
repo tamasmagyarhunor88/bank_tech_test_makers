df71454ac9f5de60fba4f606d624faa6f9c363c7
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-03-29 10:01:06 +0100

    Start separating concerns

--- lib/account.rb
+++ lib/account.rb
@@ -1,21 +1,2 @@
 class Account
-  attr_reader :balance, :logs
-
-  def initialize
-    @balance = 0
-    @logs = []
-  end
-
-  def deposit(amount)
-    @balance += amount
-    @logs << {:time => Time.now.strftime("%d/%m/%Y"), :type => :deposit, :amount => amount, :actual_balance => @balance}
-  end
-
-  def withdraw(amount)
-    @balance -= amount
-    @logs << {:time => Time.now.strftime("%d/%m/%Y"), :type => :withdraw, :amount => amount, :actual_balance => @balance}
-  end
-
-  def print_statement
-  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,55 +1,5 @@
 require 'account'
 
 describe Account do
-  before(:all) do
-    @my_account = Account.new
-    @balance = @my_account.balance
-  end
-
-  context '#initialize' do
-    it 'responds to #balance' do
-      expect(@my_account).to respond_to(:balance)
-    end
-
-    it 'responds to #logs' do
-      expect(@my_account).to respond_to(:logs)
-    end
-  end
-
-  context '#deposit' do
-    it 'responds to #deposit' do
-      expect(@my_account).to respond_to(:deposit)
-    end
-
-    it 'can #deposit amount to account' do
-      expect { @my_account.deposit(100) }.to change { @my_account.balance }.by(100)
-    end
-
-    it 'stores #deposit transaction log to #logs instance variable' do
-      @my_account.deposit(100)
-      expect(@my_account.logs).to include({:time=>"#{Time.now.strftime("%d/%m/%Y")}", :type=>:deposit, :amount=>100, :actual_balance=>200})
-    end
-  end
-
-  context '#withdraw' do
-    it 'responds to #withdraw' do
-      expect(@my_account).to respond_to(:withdraw)
-    end
-
-    it 'can #withdraw amount from account' do
-      expect { @my_account.withdraw(50) }.to change { @my_account.balance }.by(-50)
-    end
-
-    it 'stores #withdraw transaction log to #logs instance variable' do
-      @my_account.withdraw(30)
-      expect(@my_account.logs).to include({:time=>"#{Time.now.strftime("%d/%m/%Y")}", :type=>:withdraw, :amount=>30, :actual_balance=>120})
-    end
-  end
-
-  context '#print_statement' do
-    it 'responds to #print_statement' do
-      expect(@my_account).to respond_to(:print_statement)
-    end
-  end
 
 end
